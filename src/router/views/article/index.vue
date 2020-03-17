b3a16a05d1d78d113b9425eff1b1e2348437362b
tujiale <tujiale@outlook.com>
2020-03-17 21:33:18 +0800

    架构及登录

---
+++ src/router/views/article/index.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class='container'>
+    <!-- 导航 显示返回箭头-->
+    <!-- click-left点击左侧事件 -->
+    <!-- $router.go(-1) $router.back() -->
+    <van-nav-bar title='文章详情'  left-arrow @click-left="$router.back()"></van-nav-bar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
